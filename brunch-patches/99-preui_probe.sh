c2f77c5a492c174501bbc29641408c7b25034a84
ZProLegend007 <97338659+ZProLegend007@users.noreply.github.com>
2023-10-23 21:00:11 +0800

    Bluetooth fix test. # 3

--- brunch-patches/99-preui_probe.sh
+++ brunch-patches/99-preui_probe.sh
@@ -5,7 +5,7 @@ cat >/roota/etc/init/preui-probe.conf <<PREUIPROBE
 start on starting boot-services
 
 script
-	udevadm trigger --action=add --subsystem-match=pci --subsystem-match=usb --subsystem-match=i2c --subsystem-match=hid --subsystem-match=iio
+	udevadm trigger --action=add --subsystem-match=pci --subsystem-match=usb --subsystem-match=i2c --subsystem-match=hid --subsystem-match=iio --subsystem-match=uart --subsystem-match=tty --subsystem-match=rfkill
 end script
 PREUIPROBE
 if [ ! "$?" -eq 0 ]; then ret=$((ret + (2 ** 0))); fi
