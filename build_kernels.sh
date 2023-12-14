6882d0cc15276cce2a4918d2d1e69a566530f458
ZProLegend007 <97338659+ZProLegend007@users.noreply.github.com>
2023-12-14 16:34:57 +0800

    Test.
    Don't patch hci_uart.h

--- build_kernels.sh
+++ build_kernels.sh
@@ -13,7 +13,7 @@ for kernel in $kernels; do
 	mv kernel-patches/btbcm.h kernels/chromebook-6.1/drivers/bluetooth/btbcm.h | echo "btbcm.h - Success."
 	mv kernel-patches/btbcm.c kernels/chromebook-6.1/drivers/bluetooth/btbcm.c | echo "btbcm.c - Success."
 	mv kernel-patches/hci_bcm.c kernels/chromebook-6.1/drivers/bluetooth/hci_bcm.c | echo "hci_bcm.c - Success."
-	mv kernel-patches/hci_uart.h kernels/chromebook-6.1/drivers/bluetooth/hci_uart.h | echo "hci_uart.h - Success."
+#	mv kernel-patches/hci_uart.h kernels/chromebook-6.1/drivers/bluetooth/hci_uart.h | echo "hci_uart.h - Success."
 	mv kernel-patches/hci_serdev.c kernels/chromebook-6.1/drivers/bluetooth/hci_serdev.c | echo "hci_serdev.c - Success."
 	mv kernel-patches/hci_ldisc.c kernels/chromebook-6.1/drivers/bluetooth/hci_ldisc.c | echo "hci_ldisc.c - Success."
 	echo "Building kernel $kernel"
